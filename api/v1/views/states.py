b2a8b99410af713e989de4e99a6b15fd3c06302c
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-27 05:00:07 +0200

    [push] project tasks[6]

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""State Api"""
+from flask import jsonify, abort, make_response
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+
+
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def get_states():
+    """method retrieves state objects in a list"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state_id(state_id):
+    """method retrieves state object based on id or 404 if not found"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>")
+def delete_state(state_id):
+    """method deletes state based on id or 404 if not found"""
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+    else:
+        abort(404)
+    return make_response(jsonify({}), 200)
--- main.py
+++ main.py
@@ -4,8 +4,14 @@
 import models
 from models.state import State
 
-print("All objects: {}".format(models.storage.count()))
-print("State objects: {}".format(models.storage.count(State)))
+# print("All objects: {}".format(models.storage.count()))
+# print("State objects: {}".format(models.storage.count(State)))
 
-first_state_id = list(models.storage.all(State).values())[0].id
-print("First state: {}".format(models.storage.get(State, first_state_id)))
+# first_state_id = list(models.storage.all(State).values())[0].id
+# print("First state: {}".format(models.storage.get(State, first_state_id)))
+
+def func():
+    states = models.storage.all(State).values()
+    return states
+
+print(func())
