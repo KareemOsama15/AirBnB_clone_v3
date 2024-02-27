622241b0a84c9dd9e2134c2d1037f90745af42cc
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-27 06:36:35 +0200

    [push] project tasks[6]

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """State Api"""
-from flask import jsonify, abort, make_response
+from flask import jsonify, abort, make_response, request
 from api.v1.views import app_views
 from models.state import State
 from models import storage
@@ -33,3 +33,39 @@ def delete_state(state_id):
     else:
         abort(404)
     return make_response(jsonify({}), 200)
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def create_state():
+    """method create a new state instance"""
+    state_data = request.get_json()
+    if not state_data:
+        abort(400, "Not a JSON")
+    if 'name' not in state_data:
+        abort(400, "Missing name")
+
+    new_state = State(**state_data)
+    storage.new(new_state)
+    storage.save()
+    return make_response(jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """method update a state instance based on id"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(400)
+
+    state_data = request.get_json()
+    if not state_data:
+        abort(400, "Not a JSON")
+    if 'name' not in state_data:
+        abort(400, "Missing name")
+
+    ignored_attr = ["created_at", "updated_at", "id"]
+    for key, value in state_data.items():
+        if key not in ignored_attr:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
