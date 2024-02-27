574ff5675c8f42643e0e77cfe5bd9021c3459fb6
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-27 05:04:41 +0200

    [push] project tasks[6]

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,7 +22,7 @@ def get_state_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route("/states/<state_id>")
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
     """method deletes state based on id or 404 if not found"""
     state = storage.get(State, state_id)
