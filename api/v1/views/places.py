781981e9504ee7c8df9a8e0938df3b51846c3581
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-28 15:54:18 +0200

    [push] project tasks[10,11]

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,9 @@ from api.v1.views import app_views
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.user import User 
 from models import storage
 
 
